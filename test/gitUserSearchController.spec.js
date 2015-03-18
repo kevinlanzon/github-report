50bc6bcfd202aa0ed1c54e7a8f90b6a6711f4c0c
Kevin Lanzon <kevinlanzon@gmail.com>
2015-03-18 11:55:00 +0000

    first test passing

---
+++ test/gitUserSearchController.spec.js
@@ -0,0 +1,16 @@
+describe('GitUserSearchController', function() {
+  beforeEach(module('GitUserSearch'));
+
+  var scope, ctrl;
+
+  beforeEach(inject(function($rootScope, $contoller) {
+    scope = $rootScope.$new();
+    ctrl = $controller('GitUserSearchController', {
+      $scope: scope
+  });
+    it('initialises with an empty search result and term', function() {
+      expect(scope.searchResult).toBeUndefined();
+      expect(scope.searchTerm).toBeUndefined();
+    });
+  }));
+});
